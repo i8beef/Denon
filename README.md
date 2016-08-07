222549a14da19fa602d939055afdc044dc49b92b
Michael Hallock <mike@hallock.net>
2016-08-07 00:42:59 -0400

    Added NuGet supporting stuff and added badges to readme.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # I8Beef.Denon
 
+[![Build status](https://ci.appveyor.com/api/projects/status/3yigyj4fe01leceq/branch/release?svg=true)](https://ci.appveyor.com/project/i8beef/denon/branch/release)
+[![Build status](https://ci.appveyor.com/api/projects/status/3yigyj4fe01leceq/branch/master?svg=true)](https://ci.appveyor.com/project/i8beef/denon/branch/master)
+
 I8Beef.Denon is a library for interacting with Denon receivers.
 
 ### Where to get it
--- src/I8Beef.Denon/Properties/AssemblyInfo.cs
+++ src/I8Beef.Denon/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("I8Beef.Denon")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for interacting with Denon receivers.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Hallock")]
 [assembly: AssemblyProduct("I8Beef.Denon")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
